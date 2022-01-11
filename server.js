2fe4a83b32fb79cd59d4d261ccfb9f6f93fc0b65
Curtis <hong.curtis@gmail.com>
2022-01-11 07:37:19 -0800

    update code

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.listen(PORT, ()=> {
-console.log(`API server now on port ${PORT}!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
 
 const{animals} = require('./data/animals');
