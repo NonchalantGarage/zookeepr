836d5c02d46e0e325981268118c2f56046cf20ab
Curtis <hong.curtis@gmail.com>
2022-01-15 13:58:27 -0800

    update path

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -11,12 +13,21 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 // parse income JSON data
 app.use(express.json());
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
 
+
+
 const{animals} = require('./data/animals');
+const { application } = require('express');
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -81,6 +92,16 @@ app.get('/api/animals', (req, res) =>{
         results = filterByQuery(req.query,results);
     }
     res.json(results);
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post('/api/animals', (req, res) =>{
